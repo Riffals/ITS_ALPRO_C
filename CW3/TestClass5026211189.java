ed47948b247c7e96c7fd13325284fadcefb3aa7a
Riffals <99295884+Riffals@users.noreply.github.com>
2022-03-01 18:46:59 +0700

    Update TestClass5026211189.java

--- CW3/TestClass5026211189.java
+++ CW3/TestClass5026211189.java
@@ -5,8 +5,7 @@ class TestClass5026211189   {
         //celcius to fahrenheit
         System.out.println("Temp in celcius is " + cm.fToC(101.0));
         //hypotenuse
-        System.out.prin
-        tln("Hypotennuse is " + cm.hypotenuse(6 , 8));
+        System.out.println("Hypotennuse is " + cm.hypotenuse(6 , 8));
         //dice
         System.out.println("The sum of the dice values is " + cm.roll());
     }
